c5535e44f4eab7f9c77b170ada2c26ba30ca322d
Larisa <laserova@yandex.ru>
2023-09-04 01:41:33 +0500

    version conflict

---
+++ src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
@@ -0,0 +1,59 @@
+package ru.hogwarts.school.service.impl;
+
+import ru.hogwarts.school.exception.FacultyException;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FacultyServiceImpl implements FacultyService {
+    private final Map<Long, Faculty> facultyMap = new HashMap<>();
+    private long counter;
+
+
+    @Override
+    public Faculty create(Faculty faculty) {
+        if(facultyMap.containsValue(faculty)){
+            throw new FacultyException("The faculty is already in the database");
+        }
+        faculty.setId(++counter);
+        facultyMap.put(faculty.getId(),faculty);
+        return faculty;
+    }
+
+    @Override
+    public Faculty read(long id) {
+        if(!facultyMap.containsKey(id)){
+            throw new FacultyException("This faculty was not found in the database");
+        }
+        return facultyMap.get(id);
+    }
+
+    @Override
+    public Faculty update(Faculty faculty) {
+        if(!facultyMap.containsKey(faculty.getId())){
+            throw new FacultyException("This faculty was not found in the database");
+        }
+        facultyMap.put(faculty.getId(),faculty);
+        return faculty;
+    }
+
+    @Override
+    public Faculty delete(long id) {
+        Faculty faculty = facultyMap.remove(id);
+        if(faculty==null){
+            throw new FacultyException("This faculty was not found in the database");
+        }
+        return faculty;
+    }
+
+    @Override
+    public List<Faculty> readAll (String color){
+        return facultyMap.values().stream()
+                .filter(f -> f.getColor().equals(color))
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
@@ -0,0 +1,60 @@
+package ru.hogwarts.school.service.impl;
+
+import ru.hogwarts.school.exception.StudentException;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StudentServiceImpl implements StudentService {
+
+    private final Map<Long,Student> studentMap = new HashMap<>();
+    private long counter;
+
+
+    @Override
+    public Student create(Student student) {
+        if(studentMap.containsValue(student)){
+            throw new StudentException("The student is already in the database");
+        }
+        student.setId(++counter);
+        studentMap.put(student.getId(),student);
+        return student;
+    }
+
+    @Override
+    public Student read(long id) {
+        if(!studentMap.containsKey(id)){
+            throw new StudentException("This student was not found in the database");
+        }
+        return studentMap.get(id);
+    }
+
+    @Override
+    public Student update(Student student) {
+        if(!studentMap.containsKey(student.getId())){
+            throw new StudentException("This student was not found in the database");
+        }
+        studentMap.put(student.getId(),student);
+        return student;
+    }
+
+    @Override
+    public Student delete(long id) {
+        Student student = studentMap.remove(id);
+        if(student==null){
+            throw new StudentException("This student was not found in the database");
+        }
+        return student;
+    }
+
+    @Override
+    public List<Student> readAll (int age){
+        return studentMap.values().stream()
+                .filter(st ->st.getAge()==age)
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
